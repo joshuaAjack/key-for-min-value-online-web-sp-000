7e3b7c16e38d7c1f9172520dbb61f52a41c02060
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-01 04:08:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,11 @@ hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
  low = nil
- low_name = nil
+ lowest_name = nil
  name_hash.each do |key, value|
     if low == nil || low > value
       low = value
-      low_name = key
+      lowest_name = key
     end
   end
   low_name
